27b11bb07660a9fa09d0760c00ea0efd5ac86635
annotshy04 <vutruonganhyhy@gmail.com>
2023-06-12 14:29:19 +0700

    update

---
+++ 2023/n00bzCTF/RE/EZrev/EZrev.py
@@ -0,0 +1,21 @@
+# http://www.javadecompilers.com/result
+
+arr = [130, 37, 70, 115, 64, 106, 143, 34, 54, 134, 96, 98, 125, 98, 138, 104, 25, 3, 66, 78, 24, 69, 91, 80, 87, 67, 95, 8, 25, 22, 115]
+n = len(arr)
+for i in range(0, n // 2):
+    if i % 2 != 0: 
+        arr[i] = arr[i] - 30
+    else:
+        x = arr[i]
+        arr[i] = arr[n - i - 1] + 10
+        arr[n - i - 1] = x - 20
+
+for i in range(0, n):
+    if i % 2 == 0:
+        arr[i] = arr[i] ^ 0x13
+    else:
+        arr[i] = arr[i] ^ 0x37
+
+print("".join(chr(x) for x in arr))
+
+# n00bz{r3v_1s_s0_e4zy_r1ght??!!}
---
+++ 2023/n00bzCTF/RE/reverse-engineering.md
@@ -0,0 +1,18 @@
+# Welcome
+
+## Description
+
+`Can you help me pass the right input to get the flag?`
+
+## Solution
+
+Just use `strings` command to search for strings in given binary
+
+Flag: `n00bz{N3v3R_$torE_$ENs1TIV3_1nFOrMa7IOn_P1aiNtexT_In_yoUr_bin4rI3S!!!!!}`
+
+# Mypin
+
+## Description
+
+`I made a safe with a pin of only two digits.`
+
